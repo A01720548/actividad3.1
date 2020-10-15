2dc87b95e5b4176b6b738b1cb7b0df60ad14356d
Esteban de la Maza <edelamaza@me.com>
2020-10-14 21:38:39 -0500

    added test cases

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int main() {
     BinarySearchTree myTree = BinarySearchTree();;
+
+    //Casos de prueba
+    //Insert
     myTree.insert(50);
     myTree.insert(20);
     myTree.insert(65);
@@ -14,16 +17,23 @@ int main() {
     myTree.insert(70);
     myTree.insert(5);
 
-    //cout << endl << myTree.getSize();
+
+    //Get Size
+    cout << endl << myTree.getSize();
+
+    //Print Tree Preorder
     myTree.printPreOrder();
 
+    //Tree Height
     int height = myTree.height();
 
     cout << "Height: " << height << endl;
-
+    // What Level Am I
     int level = myTree.whatLevelAmI(5);
 
     cout << "Level: " << level << endl;
+
+    //Ancestors
     myTree.ancestors(5);
 
 
